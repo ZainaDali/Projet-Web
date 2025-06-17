1db814a7f3b0ea21df600ddba01716cfca2838d5
rayane rostane <rayane.rostane@universite-paris-saclay.fr>
2025-06-17 18:23:02 +0200

    fix

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -22,8 +22,9 @@ import { PingModule } from './graphql/ping.module';
     }),
     BullModule.forRoot({
       redis: {
-        host: 'localhost',
-        port: 6379,
+        host: 'redis-11095.c280.us-central1-2.gce.redns.redis-cloud.com',
+        port: 11095,
+        password: "S7yUCTW10bSgwwiEYWF2liYb4h6kcGmk",
       },
     }),
     PrismaModule,
